d88cc9d2ae7628a9d04a6c9ac8b131c459dfe156
Maria <sdfghj>
2022-02-05 19:02:39 +0300

    ввод данных

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 
 
-SCREEN_SIZE = [450, 450]
+SCREEN_SIZE = [450, 600]
 
 
 class Example(QWidget):
@@ -14,9 +14,10 @@ class Example(QWidget):
         super().__init__()
         self.getImage()
         self.initUI()
+        self.x, self.y = 37.620070, 55.753630
 
     def getImage(self):
-        map_request = "https://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&size=450,450&z=13&l=map&pt=37.620070,55.753630,pmwtm1~37.64,55.76363,pmwtm99"
+        map_request = f"https://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}&size=450,450&z=13&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -33,15 +34,25 @@ class Example(QWidget):
         self.setGeometry(600, 250, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
-
+        self.btn = QPushButton('Добавить координаты', self)
+        self.btn.move(70, 500)
+        self.btn.resize(140, 30)
+        self.btn.clicked.connect(self.getcoord)
+        self.edit_x = QLineEdit(self)
+        self.edit_x.move(10, 470)
+        self.edit_y = QLineEdit(self)
+        self.edit_y.move(160, 470)
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
-        self.image.resize(600, 450)
+        self.image.resize(450, 450)
         self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
+    def getcoord(self):
+        self.x, self.y = self.edit_x.text(), self.edit_y.text()
+        self.getImage()
 
+    def closeEvent(self, event):
         os.remove(self.map_file)
 
 
